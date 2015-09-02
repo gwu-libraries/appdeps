1a28fe1b01795b16b50fccd0f1522a76b607e29b
Justin Littman <justinlittman@gwu.edu>
2015-09-02 08:00:44 -0400

    Prepared for submitting to pypi.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='appdeps',
+    version='1.0.0',
+    url='https://github.com/gwu-libraries/appdeps',
+    author='Justin Littman',
+    author_email='justinlittman@gmail.com',
+    scripts=['appdeps.py', ],
+    description="Simple commandline tool to check and/or wait for application dependencies.",
+    platforms=['POSIX'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 5 - Production/Stable',
+    ],
+)
